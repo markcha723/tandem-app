cadd572ce215316fa53f8392af2fe1ea0af2be7c
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-07-07 16:04:23 -0600

    Change default state in App to login component, destructure Button props

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-const Button = (props) => {
+const Button = ({ color, size, onClick }) => {
   return (
     <button
-      className={`${classes.button} ${classes[props.color]} ${
-        props.size === "small" ? classes.small : classes.large
+      className={`${classes.button} ${classes[color]} ${
+        size === "small" ? classes.small : classes.large
       }`}
       onClick={props.onClick}
     >
       {props.children}
     </button>
   );
+  // onClick - dynamic? diff context for each button
+  // props.children - within JSX tag
 };
 
 export default Button;
